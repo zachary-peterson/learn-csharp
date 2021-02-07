88f7e0d1f3f43e3a39cd14aad55d14a49b0e224d
Zachary <zp.lambda@yahoo.com>
2021-02-07 12:45:40 -0500

    init

---
+++ unity2d/Number Wizard/Assets/NumberWizard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int maxGuess;
+    int minGuess;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        maxGuess = 1000;
+        minGuess = 1;
+        guess = 500;
+
+        Debug.Log("Welcome to number Wizard");
+        Debug.Log("Pick a number");
+        Debug.Log("Highest number: " + maxGuess);
+        Debug.Log("Lowest number: " + minGuess);
+        Debug.Log("Is your number Higher or Lower than " + guess + "?");
+        Debug.Log("Push UP for higher, push DOWN for lower, ENTER for correct");
+        maxGuess = maxGuess + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            minGuess = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maxGuess = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+            StartGame();
+        }
+    }
+    void NextGuess()
+    {
+            guess = (maxGuess + minGuess) / 2;
+            Debug.Log("Is it higher or lower than " + guess + "?");
+    }
+}
---
+++ unity2d/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    // int[] oddNumbers = { 1, 3, 5, 7, 9 };
+    // string[] days = { "Mon", "Tues", "Weds" };
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        // Debug.Log(oddNumbers[3]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState() 
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
